46b09594ceb6fc71beeb2b407981e34e10d63434
FILIN <hagi_beter@mail.ru>
2023-04-23 12:25:32 +0300

    Work2

---
+++ Work2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+string str = Console.ReadLine();
+int sum = 0;
+
+
+for (int i = 0; i < str.Length; i++)
+{
+    int currentNumber = int.Parse(str[i].ToString());
+    sum += currentNumber; // sum = sum + currentNumber
+}
+Console.WriteLine(sum);
+
+
+
